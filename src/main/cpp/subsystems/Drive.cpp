80b58c8cda8bb8b94253bbc3dccb763a2493f3af
JHazelwood1 <91392667+JHazelwood1@users.noreply.github.com>
2022-01-09 15:47:57 -0800

    nonfunctional update to drive code

--- src/main/cpp/subsystems/Drive.cpp
+++ src/main/cpp/subsystems/Drive.cpp
@@ -1,3 +1,39 @@
 #include "src/subsystems/Drive.h"
 
+namespace frc973{
 
+void Drive::Update() {
+    array<double, 4> outputs;
+
+    switch (m_driveMode) {
+        case DriveMode::arcade:
+
+            break;
+        case DriveMode::position:
+           
+            break;
+        case DriveMode::cheesyDrive:
+           
+            break;
+        default:
+            outputs = {0.0, 0.0};
+    }
+
+    m_leftOutput = std::clamp(outputs[Outputs::leftThrottle] + outputs[Outputs::leftTurn], m_minSpeed, m_maxSpeed);
+    m_rightOutput = std::clamp(outputs[Outputs::rightThrottle] + outputs[Outputs::rightTurn], m_minSpeed, m_maxSpeed);
+
+    m_leftOutput *= MAX_TICKS_PER_100_MS;
+    m_rightOutput *= MAX_TICKS_PER_100_MS;
+
+    CheckForIsNaN();
+
+    SetVelocity(m_leftOutput, m_rightOutput);
+    SmartDashboard::PutNumber("Left Distance", GetLeftPosition());
+    SmartDashboard::PutNumber("Right Distance", GetRightPosition());
+}
+
+
+
+
+
+} //namespace frc973 
--- src/main/include/src/subsystems/Drive.h
+++ src/main/include/src/subsystems/Drive.h
@@ -1,7 +1,48 @@
 #pragma once
 
+#include "lib/bases/Subsystem.h"
+
 namespace frc973{
 
+/**
+ * The drive subsystem mode.
+ */
+enum class DriveMode
+{
+    arcade,               /**< The arcade drive mode. */
+    position,             /**< The position drive mode. */
+    cheesyDrive,          /**< The cheesy drive mode. */
+};
+
+class Drive : public Subsystem {
+public:
+    /**
+     * Construct the Drive Subsystem.
+     * @param leftDriveTalonA The first left drive TalonFX/Falcon.
+     * @param leftDriveTalonB The second left drive TalonFX/Falcon.
+     * @param leftDriveTalonC The third left drive TalonFX/Falcon.
+     * @param rightDriveTalonA The first right drive TalonFX/Falcon.
+     * @param rightDriveTalonB The second right drive TalonFX/Falcon.
+     * @param rightDriveTalonC The third right drive TalonFX/Falcon.
+     * @param gyro The gyro.
+     * */
+    Drive();
+
+    /**
+     * Updates this subsystem.
+     */
+    void Update() override;
+
+    /**
+     * Updates the dashboard every robot cycle.
+     */
+    void DashboardUpdate() override;
+
+private:
+
+    DriveMode m_driveMode;
+
+};
 
 
 
