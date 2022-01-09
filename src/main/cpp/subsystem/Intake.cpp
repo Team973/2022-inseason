734ec88033c4f77dca1d0e311353fe5dd61a65fe
PrestonHopper <90113149+PrestonHopper@users.noreply.github.com>
2022-01-09 14:52:17 -0800

    Created Intake Subsystem

---
+++ src/main/cpp/subsystem/Intake.cpp
@@ -0,0 +1,22 @@
+#include "src/subsystems/Intake.h" 
+
+namespace frc973 {
+    
+Intake::Intake()
+    :m_intakeState(IntakeState::Retract) {
+}
+
+void Intake::Deploy() {
+}
+
+void Intake::Retract(){
+}
+
+void Intake::Update() {
+}
+
+void Intake::DashboardUpdate() {
+}
+
+} // namespace frc973
+
---
+++ src/main/include/src/subsystems/Intake.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "lib/bases/Subsystem.h"
+
+namespace frc973 {
+
+class Intake : public Subsystem {
+
+public:
+    /** 
+     * Constructs the intake subsystem 
+     */
+    Intake();
+
+    enum class IntakeState {
+        Deploy, /* Deploys the Intake */
+        Retract /* Retracts the Intake */
+    };
+
+     /** 
+     * Deploys the Intake
+     */
+    void Deploy();
+
+    /** 
+     * Retracts the Intake
+     */
+    void Retract();
+
+    /** 
+     * Updates the intake subsystem 
+     */
+    void Update();
+
+    /** 
+     * Updates the intake subsystem in Smartdashboard
+     */
+    void DashboardUpdate();
+  
+private: 
+    IntakeState m_intakeState;
+};
+  
+} // namespace frc973
