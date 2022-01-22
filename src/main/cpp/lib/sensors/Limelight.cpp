a8c3435d9b9e6161cce8dc6f2db2a914dd934ef8
Charlotte Maples <cgmaples13@gmail.com>
2022-01-21 17:45:57 -0800

    added some more functions to limelight

--- src/main/cpp/lib/sensors/Limelight.cpp
+++ src/main/cpp/lib/sensors/Limelight.cpp
@@ -6,6 +6,50 @@ Limelight::Limelight(const char *name) {
 }
 
 void Limelight::SetPipelineMode(PipelineMode mode) {
+    switch (mode) {
+        case PipelineMode::driverCamera:
+            m_limelightSensor->PutNumber("pipeline", 0);
+            break;
+        case PipelineMode::pipeline_1:
+            m_limelightSensor->PutNumber("pipeline", 1);
+            break;
+        case PipelineMode::pipeline_2:
+            m_limelightSensor->PutNumber("pipeline", 2);
+            break;
+        case PipelineMode::pipeline_3:
+            m_limelightSensor->PutNumber("pipeline", 3);
+            break;
+        case PipelineMode::pipeline_4:
+            m_limelightSensor->PutNumber("pipeline", 4);
+            break;
+        default:
+            m_limelightSensor->PutNumber("pipeline", 0);
+            break;
+    }
+}
+
+void Limelight::SetCameraDriver() {
+    SetPipelineMode(PipelineMode::driverCamera);
+}
+
+double Limelight::GetPipeline() {
+    return m_limelightSensor->GetNumber("pipeline", 97.3);
+}
+
+double Limelight::GetXOffset() {
+    return m_limelightSensor->GetNumber("tx", 0.0);
+}
+
+double Limelight::GetYOffset() {
+    return m_limelightSensor->GetNumber("ty", 0.0);
+}
+
+double Limelight::GetTargetArea() {
+    return m_limelightSensor->GetNumber("ta", 0.0);
+}
+
+void Limelight::GetHorizontalDist() {
+    // TODO: calculate horizontal distance from limelight to goal
 }
     
 } // namespace frc973
--- src/main/include/lib/sensors/Limelight.h
+++ src/main/include/lib/sensors/Limelight.h
@@ -2,7 +2,9 @@
 
 #include <iostream>
 
-#include <networktables/NetworkTableInstance.h>
+#include <networktables/NetworkTable.h>
+
+#include "lib/Constants.h"
 
 namespace frc973 {
 /**
@@ -20,11 +22,11 @@ public:
      */
     enum class PipelineMode
     {
-        pipeline_0,            
-        pipeline_1,       
-        pipeline_2,        
-        pipeline_3, 
-        pipeline_4  
+        driverCamera,      /**< The regular camera, no reflection. Pipeline 0 */           
+        pipeline_1,         /**< Pipeline 1 */  
+        pipeline_2,         /**< Pipeline 2 */ 
+        pipeline_3,         /**< Pipeline 3 */ 
+        pipeline_4          /**< Pipeline 4 */ 
     };
 
     /**
@@ -32,9 +34,50 @@ public:
      */ 
     void SetPipelineMode(PipelineMode mode);
 
+    /**
+     * Sets the pipeline to the one with driver camera (pipeline 0)
+     */
+    void SetCameraDriver();
+
+    /**
+     * Gets the pipeline that is currently used
+     * @return the current pipeline
+     */ 
+    double GetPipeline();
+    
+    /**
+     * Gets target X offset in degrees
+     * @return target X offset
+     */ 
+    double GetXOffset();
+
+    /**
+     * Gets target Y offset in degrees
+     * @return target Y offset
+     */ 
+    double GetYOffset();
+
+    /**
+     * Gets the target area
+     * @return the target area
+     */ 
+    double GetTargetArea();
+
+     /**
+      * Gets the distance from the limelight to the target in the horizontal direction
+      */
+    void GetHorizontalDist(); 
+
 private:
-    double m_limelightSensor;
+    std::shared_ptr<nt::NetworkTable> m_limelightSensor;
 
     PipelineMode m_pipelineMode;
+
+    static constexpr double TARGET_HEIGHT = 1; /**< Target's height in inches from ground TODO: UPDATE VALUE*/
+    static constexpr double CAMERA_HEIGHT = 1; /**< Camera's Height in inches from ground TODO: UPDATE VALUE*/
+    static constexpr double CAMERA_ANGLE = 
+            1 * (Constants::PI / 180.0); /**< Camera Angle in degrees wrt ground TODO: UPDATE VALUE*/
+            // ### for comp; ### for pbot
+    static constexpr double CAMERA_BUMPER_OFFSET = 1; /**< The offset in inches between the camera and the bumper edge TODO: UPDATE VALUE*/
 };  
 } // namespace frc973
