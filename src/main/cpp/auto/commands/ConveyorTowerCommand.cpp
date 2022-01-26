7581769ce37a50057498e73d6a2bc1747169965a
williamnotwill <absolutelynowillhere@gmail.com>
2022-01-25 20:35:36 -0800

    conveyor tower and floor commands

---
+++ src/main/cpp/auto/commands/ConveyorTowerCommand.cpp
@@ -0,0 +1,24 @@
+#include "src/auto/commands/ConveyorTowerCommand.h"
+
+namespace frc973 {
+
+ConveyorTowerCommand::ConveyorTowerCommand(Conveyor *conveyor, Conveyor::TowerState towerState, uint32_t targetTime)
+        : m_conveyorTowerState(towerState), m_targetTime(targetTime) {
+}
+
+void ConveyorTowerCommand::Init() {
+    SetTargetMSec(m_targetTime);
+    m_conveyor->SetTowerState(m_conveyorTowerState);
+}
+
+void ConveyorTowerCommand::Run() {
+}
+
+bool ConveyorTowerCommand::IsCompleted() {
+    return HasElapsed();
+}
+
+void ConveyorTowerCommand::PostExecute() {
+    m_conveyor->SetTowerState(Conveyor::TowerState::Off);
+}
+}
--- src/main/include/lib/Util.h
+++ src/main/include/lib/Util.h
@@ -11,5 +11,21 @@ namespace frc973 {
 
 namespace Util {
     
+/**
+ * Get the current time in microseconds.
+ * @return The current time.
+ */
+inline uint64_t GetUsecTime() {
+    return RobotController::GetFPGATime();
+}
+
+/**
+ * Get the current time in milliseconds.
+ * @return The current time.
+ */
+inline uint32_t GetMsecTime() {
+    return GetUsecTime() * Constants::MSEC_PER_USEC;
+}
+
 } // namespace Util
 } // namespace frc973
