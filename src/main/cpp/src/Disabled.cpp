caac5334ba5aa7d8dc7c978047c36a661ba03f30
Charlotte Maples <cgmaples13@gmail.com>
2022-02-12 17:01:51 -0800

    added turret and drive

--- src/main/cpp/src/Disabled.cpp
+++ src/main/cpp/src/Disabled.cpp
@@ -10,10 +10,11 @@ void Robot::DisabledInit() {
 }
 
 void Robot::DisabledPeriodic() {
+    // lights
     bool foundHome = false;
 
-    if(foundHome == false) {
-        if(!m_turretSensor->Get()) {
+    if (foundHome == false) {
+        if (!m_turretSensor->Get()) {
             foundHome = true;
             m_turret->SetHomeOffset();
             m_lights->SetLightsState(Lights::LightsState::Middle);
@@ -21,7 +22,28 @@ void Robot::DisabledPeriodic() {
         }
     }
 
-    
-    
+    // turret
+    m_turret->Turn(
+        m_turret->CalcJoystickAngleInDegrees(-m_operatorStick->GetRawAxis(5), -m_operatorStick->GetRawAxis(4)),
+        m_gyro->GetWrappedAngle());
+
+    if (m_operatorStick->GetLeftBumper()) {
+        m_limelight->SetVisionCamera();
+
+        // m_turret->CalcTransitionalCompensations(m_drive->GetVelocity(), 36.0);
+
+        // SmartDashboard::PutNumber("Velocity", m_drive->GetVelocity());
+
+        m_turret->CalcOutput(m_limelight->GetXOffset(), m_gyro->GetAngularRate());
+        // m_turret->CalcOutput(0, 0);
+
+        // SmartDashboard::PutNumber("Distance", m_limelight->GetHorizontalDist());
+    } else {
+        m_limelight->SetCameraDriver();
+    }
+
+    // drive
+    m_drive->SetThrottleTurn(m_driverStick->GetRawAxisWithDeadband(1, false, 0.05),
+                             m_driverStick->GetRawAxisWithDeadband(2, false, 0.05));
 }
 }  // namespace frc973
