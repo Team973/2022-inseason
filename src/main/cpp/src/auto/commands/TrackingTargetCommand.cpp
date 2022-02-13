cbee1f1934a80128fa7b0103eb5a40379ba43b59
williamnotwill <absolutelynowillhere@gmail.com>
2022-02-13 14:34:54 -0800

    made run function do things

--- src/main/cpp/src/auto/commands/TrackingTargetCommand.cpp
+++ src/main/cpp/src/auto/commands/TrackingTargetCommand.cpp
@@ -3,23 +3,23 @@
 namespace frc973 {
 
 TrackingTargetCommand::TrackingTargetCommand(Limelight *limeLight, Turret *turret, Gyro *gyro, uint32_t targetTime)
-        : m_limeLight(limeLight), m_turret(turret), m_gyro(gyro), m_targetTime(targetTime) {
+        : m_limeLight(limeLight), m_turret(turret), m_gyro(gyro), m_targetTime(targetTime), m_hasElapsed(false) {
 }
 
 void TrackingTargetCommand::Init() {
-    if (-5.0 < m_limeLight->GetXOffset() < 5.0) {
-        IsCompleted();
-    } else {
-        m_turret->CalcOutput(m_limeLight->GetXOffset(), m_gyro->GetAngularRate());
-        Init();
-    }
+    m_turret->CalcOutput(m_limeLight->GetXOffset(), m_gyro->GetAngularRate());
 }
 
 void TrackingTargetCommand::Run() {
+    if (-5.0 < m_limeLight->GetXOffset() < 5.0) {
+        m_hasElapsed = true;
+    } else {
+        m_hasElapsed = false;
+    }
 }
 
 bool TrackingTargetCommand::IsCompleted() {
-    return true;
+    return m_hasElapsed;
 }
 
 void TrackingTargetCommand::PostExecute() {
