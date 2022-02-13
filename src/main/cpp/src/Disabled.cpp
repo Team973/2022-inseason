1cc259c0fa60eccb4e285a5fc41215a3783d1225
Charlotte Maples <cgmaples13@gmail.com>
2022-02-12 17:13:24 -0800

    drive, turret, shooter in teleop

--- src/main/cpp/src/Disabled.cpp
+++ src/main/cpp/src/Disabled.cpp
@@ -21,29 +21,5 @@ void Robot::DisabledPeriodic() {
             m_turret->SetNeutralMode(NeutralMode::Brake);
         }
     }
-
-    // turret
-    m_turret->Turn(
-        m_turret->CalcJoystickAngleInDegrees(-m_operatorStick->GetRawAxis(5), -m_operatorStick->GetRawAxis(4)),
-        m_gyro->GetWrappedAngle());
-
-    if (m_operatorStick->GetLeftBumper()) {
-        m_limelight->SetVisionCamera();
-
-        // m_turret->CalcTransitionalCompensations(m_drive->GetVelocity(), 36.0);
-
-        // SmartDashboard::PutNumber("Velocity", m_drive->GetVelocity());
-
-        m_turret->CalcOutput(m_limelight->GetXOffset(), m_gyro->GetAngularRate());
-        // m_turret->CalcOutput(0, 0);
-
-        // SmartDashboard::PutNumber("Distance", m_limelight->GetHorizontalDist());
-    } else {
-        m_limelight->SetCameraDriver();
-    }
-
-    // drive
-    m_drive->SetThrottleTurn(m_driverStick->GetRawAxisWithDeadband(1, false, 0.05),
-                             m_driverStick->GetRawAxisWithDeadband(2, false, 0.05));
 }
 }  // namespace frc973
