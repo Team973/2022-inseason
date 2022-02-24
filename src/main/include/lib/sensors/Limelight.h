2117090989b79d48a51eea6bb27467daef4beb8b
Charlotte Maples <cgmaples13@gmail.com>
2022-02-23 22:13:48 -0800

    updated limelight angle

--- src/main/include/lib/sensors/Limelight.h
+++ src/main/include/lib/sensors/Limelight.h
@@ -119,7 +119,7 @@ private:
         96.0; /**< Target's height in inches from ground*/
     static constexpr double CAMERA_HEIGHT = 34.8; /**< Camera's Height in inches from ground TODO: UPDATE VALUE*/
     static constexpr double CAMERA_ANGLE =
-        35.0; /**< Camera Angle in degrees wrt ground*/
+        37.0; /**< Camera Angle in degrees wrt ground*/
               // ### for comp; ### for pbot
     static constexpr double CAMERA_BUMPER_OFFSET =
         1; /**< The offset in inches between the camera and the bumper edge TODO: UPDATE VALUE - current cf*/
